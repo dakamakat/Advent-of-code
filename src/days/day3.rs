c8cceb84f8c426d4fc69ab6afd18e56bc953cbae
dakamakat <gazon-ozon@yandex.ru>
2024-07-29 15:57:13 +0200

    feat(day3): robo santa iterative turns

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -10,13 +10,7 @@ pub fn at_least_one(input: String) -> i32 {
     };
 
     for it in input.chars() {
-        place(
-            it,
-            position.height,
-            position.width,
-            &mut field,
-            &mut position,
-        )
+        place(it, &mut field, &mut position)
     }
 
     let mut sum = 0;
@@ -28,13 +22,47 @@ pub fn at_least_one(input: String) -> i32 {
     sum + 1
 }
 
-fn place(
-    chr: char,
-    rank: usize,
-    index: usize,
-    arr: &mut [[i32; 500]; 500],
-    position: &mut Position,
-) {
+pub fn at_least_one_robo(input: String) -> i32 {
+    const WIDTH: usize = 500;
+    const HEIGHT: usize = 500;
+
+    let mut field = [[0; WIDTH]; HEIGHT];
+
+    let mut santa_position = Position {
+        width: 250,
+        height: 250,
+    };
+
+    let mut robo_position = Position {
+        width: 250,
+        height: 250,
+    };
+
+    let mut santa_turn = true;
+
+    for it in input.chars() {
+        if santa_turn {
+            place(it, &mut field, &mut santa_position);
+            santa_turn = false;
+            continue;
+        }
+
+        place(it, &mut field, &mut robo_position);
+        santa_turn = true;
+    }
+
+    let mut sum = 0;
+
+    for arr in field {
+        sum += arr.iter().sum::<i32>();
+    }
+
+    sum
+}
+
+fn place(chr: char, arr: &mut [[i32; 500]; 500], position: &mut Position) {
+    let rank = position.height;
+    let index = position.width;
     if chr == '^' {
         let new_rank: usize = rank - 1;
         arr[new_rank][index] = 1;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use days::day3::at_least_one;
+use days::day3::{at_least_one, at_least_one_robo};
 
 mod days;
 mod utils;
 
 fn main() {
     let input = utils::read_signle("./src/days/inputs/day3.txt".to_string());
-    let result = at_least_one(input);
+    let result = at_least_one_robo(input);
 
     print!("{0}", result);
 }
