3129e8390694b655a528ee9f8ca8ace14a0e0782
dakamakat <gazon-ozon@yandex.ru>
2024-08-12 14:55:55 +0200

    feat(day5): tricky second part

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -50,3 +50,54 @@ pub fn intern_work(input: Vec<String>) -> i32 {
 
     count
 }
+
+pub fn intern_work_second_iteration(input: Vec<String>) -> i32 {
+    let mut count = 0;
+
+    let mut repeats = false;
+    let mut twice_appeared = false;
+
+    for string in input {
+        let b = string.as_bytes();
+        let ln = string.chars().count() - 1;
+
+        for (i, ch) in b.iter().enumerate() {
+            let first = *ch as char;
+
+            let mut next = i + 1;
+            if !repeats && next <= ln {
+                let second = b[next] as char;
+                let merged = format!("{}{}", first, second);
+
+                let mut cloned_str = string.clone();
+                cloned_str.replace_range(i..next + 1, "0");
+
+                if cloned_str.contains(&merged) {
+                    repeats = true;
+                }
+            }
+
+            next = i + 2;
+            if !twice_appeared && next <= ln {
+                let second = b[next] as char;
+
+                if first == second {
+                    twice_appeared = true;
+                }
+            }
+
+            if repeats && twice_appeared {
+                break;
+            }
+        }
+
+        if repeats && twice_appeared {
+            count += 1;
+        }
+
+        repeats = false;
+        twice_appeared = false;
+    }
+
+    count
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use days::day5::intern_work;
+use days::day5::intern_work_second_iteration;
 
 mod days;
 mod utils;
 
 fn main() {
     let input = utils::read_lines("./src/days/inputs/day5.txt");
-    let result = intern_work(input);
+    let result = intern_work_second_iteration(input);
 
     print!("{0}", result);
 }
